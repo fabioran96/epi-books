125cb4d61ab32e032424c59b69a6fd95615e413d
Fabio Ranocchiari <fabio.ranocchiari@gmail.com>
2024-06-25 16:36:47 +0200

    Update allTheBooks.js

--- src/components/allTheBooks.js
+++ src/components/allTheBooks.js
@@ -1,32 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import { Card, CardDeck, Container, Row, Col } from 'react-bootstrap';
-import booksData from '../data/scifi.json'; // Modifica il percorso in base al file JSON che vuoi caricare
+import React, {useState } from 'react';
+import { Card, Container, Row, Col, ButtonGroup, Button } from 'react-bootstrap';
+import scifiBooks from '../data/scifi.json';
+import fantasyBooks from '../data/fantasy.json';
+import horrorBooks from '../data/horror.json';
+import romanceBooks from '../data/romance.json';
+import historyBooks from '../data/history.json';
 
 const AllTheBooks = () => {
-  const [books, setBooks] = useState([]);
+    const [genre, setGenre] = useState('scifi');
+  
+    const getBooksByGenre = (genre) => {
+      switch (genre) {
+        case 'scifi':
+          return scifiBooks;
+        case 'fantasy':
+          return fantasyBooks;
+        case 'horror':
+          return horrorBooks;
+        case 'romance':
+          return romanceBooks;
+        case 'history':
+          return historyBooks;
+        default:
+          return [];
+      }
+    };
+  
+    const books = getBooksByGenre(genre);
+  
+    return (
+      <Container>
+        <ButtonGroup className="mt-4">
+          <Button onClick={() => setGenre('scifi')}>Sci-Fi</Button>
+          <Button onClick={() => setGenre('fantasy')}>Fantasy</Button>
+          <Button onClick={() => setGenre('horror')}>Horror</Button>
+          <Button onClick={() => setGenre('romance')}>Romance</Button>
+          <Button onClick={() => setGenre('history')}>History</Button>
+        </ButtonGroup>
+        <Row className="mt-4">
+          {books.map(book => (
+            <Col sm={12} md={6} lg={4} className="mb-4" key={book.asin}>
+              <Card>
+                <Card.Img variant="top" src={book.img} />
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <Card.Text>Price: ${book.price}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    );
+  };
+  
+  export default AllTheBooks;
+  
 
-  useEffect(() => {
-    // Supponendo che il file JSON sia già importato come `booksData`
-    setBooks(booksData);
-  }, []);
-
-  return (
-    <Container>
-      <Row className="mt-4">
-        {books.map(book => (
-          <Col sm={12} md={6} lg={4} className="mb-4" key={book.asin}>
-            <Card>
-              <Card.Img variant="top" src={book.img} />
-              <Card.Body>
-                <Card.Title>{book.title}</Card.Title>
-                <Card.Text>Price: ${book.price}</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </Container>
-  );
-};
-
-export default AllTheBooks;
